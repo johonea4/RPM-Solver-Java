75e12f04eef234389e249d69ea678b79f5b9bb2a
Joseph M. Honea <johonea@gatech.edu>
2017-09-14 00:58:28 -0500

    Completed Semantic Network building. Now the data needs to be sifted through for the application to create an answer.

--- Project-Code-Java/ravensproject/Agent.java
+++ Project-Code-Java/ravensproject/Agent.java
@@ -17,15 +17,15 @@ class LexicalDatabase
 {
     enum KEYS { SHAPE, ALIGNMENT, FILL, SIZE, ANGLE }
     enum RELATIONSHIPS   { UNKNOWN, ABOVE, BELOW, INSIDE, OUTSIDE, LEFT, RIGHT, FILL, SIZE, ANGLE, SELF }
-    enum SHAPES { SQUARE, CIRCLE, TRIANGLE, RECTANGLE, PENTAGON, HEXAGON, OCTAGON, DIAMOND, RIGHT_TRIANGLE }
+    enum SHAPES { SQUARE, CIRCLE, TRIANGLE, RECTANGLE, PENTAGON, HEXAGON, OCTAGON, DIAMOND, RIGHT_TRIANGLE, PAC_MAN, STAR, HEART, UNKNOWN }
     enum SIZES   { VERY_SMALL, SMALL, MEDIUM, LARGE, VERY_LARGE, HUGE };
     enum VOCAB  { NOT_DEFINED,
                   BOTTOM_RIGHT, BOTTOM_LEFT, TOP_RIGHT, TOP_LEFT, BOTTOM, TOP,
                   LEFT_HALF, RIGHT_HALF, TOP_HALF, BOTTOM_HALF, YES, NO }
-    enum TRANSLATIONS { UNKNOWN, UNCHANGED, DELETED, MOVED, ENLARGE, SHRINK, SUBTRACTED, ROTATED, MIRRORED, NEW }
+    enum TRANSLATIONS { UNKNOWN, UNCHANGED, DELETED, MOVED, ENLARGE, SHRINK, ROTATED, MIRRORED, NEW }
     public LexicalDatabase()
     {
-        database.clear();
+        database = new HashMap<>();
         for( SHAPES v : SHAPES.values()) database.put(v.name(),v);
         for( SIZES v : SIZES.values()) database.put(v.name(),v);
         for( VOCAB v : VOCAB.values()) database.put(v.name(),v);
@@ -36,8 +36,9 @@ class LexicalDatabase
 
     public Object getValue(String s)
     {
-        if(s.isEmpty()) return VOCAB.NOT_DEFINED;
+        if(s == null || s.isEmpty()) return VOCAB.NOT_DEFINED;
         s = s.replaceAll(" ", "_");
+        s = s.replaceAll("-", "_");
         s = s.toUpperCase();
         if(database.containsKey(s)) return database.get(s);
         boolean isNumber=true;
@@ -58,34 +59,52 @@ class GraphNode
 {
     public GraphNode(RavensObject obj, LexicalDatabase db)
     {
+        above = new ArrayList<>();
+        inside = new ArrayList<>();
+        left = new ArrayList<>();
+        right = new ArrayList<>();
         object = obj;
         objectName = obj.getName();
-        position = (LexicalDatabase.VOCAB)db.getValue(obj.getAttributes().get("alignment"));
-        shape = (LexicalDatabase.SHAPES)db.getValue(obj.getAttributes().get("shape"));
-        size = (LexicalDatabase.SIZES)db.getValue(obj.getAttributes().get("size"));
-        fill = (LexicalDatabase.VOCAB)db.getValue(obj.getAttributes().get("fill"));
-        angle = (double)db.getValue(obj.getAttributes().get("angle"));
+        if(object.getAttributes().containsKey(LexicalDatabase.KEYS.ALIGNMENT.name()))
+            position = (LexicalDatabase.VOCAB)db.getValue(obj.getAttributes().get("alignment"));
+        else position = LexicalDatabase.VOCAB.NOT_DEFINED;
+        if(object.getAttributes().containsKey("shape"))
+            shape = (LexicalDatabase.SHAPES)db.getValue(obj.getAttributes().get("shape"));
+        else
+            shape = LexicalDatabase.SHAPES.UNKNOWN;
+        if(object.getAttributes().containsKey("size"))
+            size = (LexicalDatabase.SIZES)db.getValue(obj.getAttributes().get("size"));
+        else
+            size = LexicalDatabase.SIZES.MEDIUM;
+        if(object.getAttributes().containsKey("fill"))
+            fill = (LexicalDatabase.VOCAB)db.getValue(obj.getAttributes().get("fill"));
+        else
+            fill = (LexicalDatabase.VOCAB.NOT_DEFINED);
+        if(object.getAttributes().containsKey("angle"))
+            angle = (double) ( db.getValue(obj.getAttributes().get("angle")));
+        else
+            angle = 0;
     }
     public void GetRelationships(HashMap<String,GraphNode> objects)
     {
-        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.ABOVE.name()))
+        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.ABOVE.name().toLowerCase()))
         {
-            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.ABOVE.name()).split(","));
+            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.ABOVE.name().toLowerCase()).split(","));
             for(String obj : list) if(objects.containsKey(obj)) above.add(objects.get(obj));
         }
-        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.INSIDE.name()))
+        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.INSIDE.name().toLowerCase()))
         {
-            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.INSIDE.name()).split(","));
+            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.INSIDE.name().toLowerCase()).split(","));
             for(String obj : list) if(objects.containsKey(obj)) inside.add(objects.get(obj));
         }
-        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.LEFT.name()))
+        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.LEFT.name().toLowerCase()))
         {
-            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.LEFT.name()).split(","));
+            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.LEFT.name().toLowerCase()).split(","));
             for(String obj : list) if(objects.containsKey(obj)) left.add(objects.get(obj));
         }
-        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.RIGHT.name()))
+        if(object.getAttributes().containsKey(LexicalDatabase.RELATIONSHIPS.RIGHT.name().toLowerCase()))
         {
-            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.RIGHT.name()).split(","));
+            List<String> list = Arrays.asList(object.getAttributes().get(LexicalDatabase.RELATIONSHIPS.RIGHT.name().toLowerCase()).split(","));
             for(String obj : list) if(objects.containsKey(obj)) right.add(objects.get(obj));
         }
         if(position != LexicalDatabase.VOCAB.NOT_DEFINED)
@@ -104,6 +123,14 @@ class GraphNode
                     if(nd.size.ordinal() > size.ordinal())
                         inside.add(nd);
         }
+    }
+
+    public int GetSimilarityScore(GraphNode nd)
+    {
+        int score=0;
+        if(shape == nd.shape) score+=10;
+        if(size == nd.size) score+=5;
+        return score;
     }
 
     public String objectName;
@@ -125,6 +152,7 @@ class FigureGraph
     {
         figureName = f.getName();
         figure = f;
+        Nodes = new HashMap<>();
 
         HashMap<String, RavensObject> nodelist = f.getObjects();
         for(RavensObject n : nodelist.values())
@@ -140,18 +168,74 @@ class FigureGraph
 //----Here Down-----> Still Working
 class TranslationConnection
 {
-    public GraphNode figure1Node;
-    public GraphNode figure2Node;
-    public int translation;
+    TranslationConnection(GraphNode n1, GraphNode n2)
+    {
+        Node1 = n1;
+        Node2 = n2;
+        translations = new ArrayList<LexicalDatabase.TRANSLATIONS>();
+
+        if(n1 == null) { translations.add(LexicalDatabase.TRANSLATIONS.NEW); return; }
+        if(n2 == null) { translations.add(LexicalDatabase.TRANSLATIONS.DELETED); return; }
+        if(n1.shape != n2.shape) translations.add(LexicalDatabase.TRANSLATIONS.DELETED);
+        if(n2.shape != n1.shape) translations.add(LexicalDatabase.TRANSLATIONS.NEW);
+        if(n1.position != n2.position) translations.add(LexicalDatabase.TRANSLATIONS.MOVED);
+        if(n1.size.ordinal() > n2.size.ordinal()) translations.add(LexicalDatabase.TRANSLATIONS.SHRINK);
+        if(n1.size.ordinal() < n2.size.ordinal()) translations.add(LexicalDatabase.TRANSLATIONS.ENLARGE);
+        if(n1.angle != n2.angle) translations.add(LexicalDatabase.TRANSLATIONS.ROTATED);
+        if(n1.shape==n2.shape && n1.position==n2.position && n1.size==n2.size && n1.angle==n2.angle && n1.inside.size()==n2.inside.size() &&
+                n1.above.size()==n2.above.size() && n1.left.size()==n2.left.size() && n1.right.size()==n2.right.size())
+            translations.add(LexicalDatabase.TRANSLATIONS.UNCHANGED);
+        if(translations.size()<=0) translations.add(LexicalDatabase.TRANSLATIONS.UNKNOWN);
+    }
+    public GraphNode Node1;
+    public GraphNode Node2;
+    public List<LexicalDatabase.TRANSLATIONS> translations;
 }
 
-class FigureTranslation
+class TranslationGraph
 {
-    public String f1Name;
-    public String f2Name;
+    public TranslationGraph(RavensFigure f1, RavensFigure f2, LexicalDatabase db)
+    {
+        figure1 = new FigureGraph(f1, db);
+        figure2 = new FigureGraph(f2, db);
+        connections = new ArrayList<TranslationConnection>();
+
+        //--- Find similar nodes across figures
+        HashMap<String, GraphNode> f1Nodes = figure1.Nodes;
+        HashMap<String, GraphNode> f2Nodes = figure2.Nodes;
+
+        for(GraphNode nd : f1Nodes.values())
+        {
+            int score=0;
+            GraphNode matchNode = null;
+            for(GraphNode nd2 : f2Nodes.values())
+            {
+                int score2 = nd.GetSimilarityScore(nd2);
+                if(score2 > score) {
+                    score = score2;
+                    matchNode = nd2;
+                }
+            }
+            connections.add(new TranslationConnection(nd,matchNode));
+        }
+        for(GraphNode nd : f2Nodes.values())
+        {
+            int score=0;
+            for(GraphNode nd1 : f1Nodes.values())
+            {
+                int score2 = nd.GetSimilarityScore(nd1);
+                if(score2 > score)
+                {
+                    score = score2;
+                    break;
+                }
+            }
+            if(score==0) connections.add(new TranslationConnection(null, nd));
+        }
+    }
     public FigureGraph figure1;
     public FigureGraph figure2;
-    public ArrayList<TranslationConnection> translations;
+    public List<TranslationConnection> connections;
 }
 
 class RPM_Graph
@@ -159,8 +243,37 @@ class RPM_Graph
     //---- 2x2 will result in A->B, C->N, A->C, B->N
     //---- 3x3 will result in A->B,B->C, D->E,E->F, G->H,H->N
     //----                    A->D,D->G, B->E,E->H, C->F,F->N
-    public int problemType;
-    public ArrayList<FigureTranslation> rpm;
+    RPM_Graph(HashMap<String, RavensFigure> figures, int ptype, LexicalDatabase db, String N)
+    {
+        ProblemType = ptype;
+        translationGraph = new HashMap<>();
+        if(ptype == 0)
+        {
+            translationGraph.put("AB",new TranslationGraph(figures.get("A"),figures.get("B"),db));
+            translationGraph.put("B" + N,new TranslationGraph(figures.get("B"),figures.get(N),db));
+            translationGraph.put("AC",new TranslationGraph(figures.get("A"),figures.get("C"),db));
+            translationGraph.put("C" + N,new TranslationGraph(figures.get("C"),figures.get(N),db));
+        }
+        if(ptype == 1)
+        {
+            translationGraph.put("AB",new TranslationGraph(figures.get("A"),figures.get("B"),db));
+            translationGraph.put("BC",new TranslationGraph(figures.get("B"),figures.get("C"),db));
+            translationGraph.put("DE",new TranslationGraph(figures.get("D"),figures.get("E"),db));
+            translationGraph.put("EF",new TranslationGraph(figures.get("E"),figures.get("F"),db));
+            translationGraph.put("GH",new TranslationGraph(figures.get("G"),figures.get("H"),db));
+            translationGraph.put("H"+N,new TranslationGraph(figures.get("H"),figures.get(N),db));
+            translationGraph.put("AD",new TranslationGraph(figures.get("A"),figures.get("D"),db));
+            translationGraph.put("DG",new TranslationGraph(figures.get("D"),figures.get("G"),db));
+            translationGraph.put("BE",new TranslationGraph(figures.get("B"),figures.get("E"),db));
+            translationGraph.put("EH",new TranslationGraph(figures.get("E"),figures.get("H"),db));
+            translationGraph.put("CF",new TranslationGraph(figures.get("C"),figures.get("F"),db));
+            translationGraph.put("F" + N,new TranslationGraph(figures.get("F"),figures.get(N),db));
+
+        }
+
+    }
+    HashMap<String, TranslationGraph> translationGraph;
+    int ProblemType;
 }
 
 class SemanticNetworkGenerator
@@ -172,55 +285,24 @@ class SemanticNetworkGenerator
         problemType = ptype;
     }
 
-    public ArrayList<RPM_Graph> generateNets()
+    public List<RPM_Graph> generateNets()
     {
-        if(problemType==0) return generateNets_2x2();
-        else return generateNets_3x3();
+        Set<String> keys = ravensfigures.keySet();
+        List<RPM_Graph> nets = new ArrayList<RPM_Graph>();
+
+        for (String k : keys)
+        {
+            if(Character.isDigit(k.charAt(0)))
+            {
+                nets.add(new RPM_Graph(ravensfigures,problemType,lexicalDatabase,k));
+            }
+        }
+        return nets;
     }
 
     private HashMap<String, RavensFigure> ravensfigures;
     private LexicalDatabase lexicalDatabase;
     private int problemType;
-
-    private FigureGraph BuildFigureRelationships(RavensFigure f)
-    {
-        HashMap<String, RavensObject> objs = f.getObjects();
-        ArrayList<GraphNode> nodes = new ArrayList<GraphNode>();
-        for(String key : objs.keySet())
-        {
-            nodes.add(new GraphNode(objs.get(key),lexicalDatabase));
-        }
-
-        for( GraphNode nd : nodes)
-        {
-            for( GraphNode nd2 : nodes)
-            {
-                if(nd == nd2) continue;
-            }
-        }
-    }
-    private ArrayList<RPM_Graph> generateNets_2x2()
-    {
-        //---Alright, First each figure needs relationships built
-
-        //---Next Find matching figures between figures
-
-        //---Next Find the translations between the figures
-
-        //---Return a list of all graphs
-    }
-
-    private ArrayList<RPM_Graph> generateNets_3x3()
-    {
-        //---Alright, First each figure needs relationships built
-
-        //---Next Find matching figures between figures
-
-        //---Next Find the translations between the figures
-
-        //---Return a list of all graphs
-    }
-
 }
 
 class StatisticalAnalyzer
@@ -275,19 +357,11 @@ public class Agent {
 
         if(problem.hasVisual() && !problem.hasVerbal()) return -1;
 
-        System.console().printf("=========================================\n");
-        System.console().printf("Starting Problem: %s\n", problem.getName());
-        System.console().printf("Problem Type: %s\n",problem.getProblemType());
-        System.console().printf("Num Figures: %d\n", problem.getFigures().size());
-        System.console().printf("=========================================\n");
 
         HashMap<String, RavensFigure> figures = problem.getFigures();
 
-        SemanticNetworkGenerator generator = new SemanticNetworkGenerator(lexicalDatabase, figures, problem.getProblemType()== "2x2" ? 0 : 1);
-
-
-
-        System.console().printf("=========================================\n");
+        SemanticNetworkGenerator generator = new SemanticNetworkGenerator(lexicalDatabase, figures, problem.getProblemType().contentEquals("2x2") ? 0 : 1);
+        List<RPM_Graph> rpms = generator.generateNets();
 
         return -1;
     }
